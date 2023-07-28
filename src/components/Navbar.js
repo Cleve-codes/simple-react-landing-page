26362e791a248c9ec8ba95c0eec7fcd58d439f86
Cleve <clevemomanyi@gmail.com>
2023-07-28 09:08:56 +0300

    Not sure

---
+++ src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+import Logo from "./Assets/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+import {
+  HiOutlineAcademicCap,
+  HiOutlineBars3,
+  hiOutlineBars3,
+} from "react-icons/hi2";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-mateerial/ShoppingCartRounded";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contact",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="The logo" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Testimonials</a>
+        <a href="#">Contact</a>
+        <a href="#">
+          <BsCart2 />
+        </a>
+        <button className="primary-button">Bookings Now</button>
+      </div>
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
