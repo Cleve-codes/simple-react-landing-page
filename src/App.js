c82826d74e7e28b9ca9cfb9207ccc09163ec2502
Cleve <clevemomanyi@gmail.com>
2023-07-30 07:45:24 +0300

    Sunday

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
 
   return (
     <div className={`App ${darkMode ? "dark-mode" : ""}`}>
+      <video autoplay loop muted plays-inline class="bg-vid">
+        <source src="./Assets/bgVid.mp4" type="video/mp4"></source>
+      </video>
       <Home handleModes={handleModes} darkMode={darkMode} />
       <Menu />
     </div>
