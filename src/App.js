8667497adcb2e4c2fe9a40a2cd2e912de79b8360
Cleve <clevemomanyi@gmail.com>
2023-07-30 22:05:14 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./index.css";
 import { useState } from "react";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
-import videoBg from "./Assets/bgVid.mp4";
+import videoBg from "./Assets/bgVid3.mp4";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
