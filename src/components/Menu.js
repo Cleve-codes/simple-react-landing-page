207ce2627445aea769ee5633b9c206b68f1b11c3
Cleve <clevemomanyi@gmail.com>
2023-07-31 10:07:25 +0300

    not yet

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Pizza from "../Assets/pizza.jpeg"
 
 const Menu = () => {
-  return <div>
-    <div className="menu-container">
-      
+  return (
+    <div>
+      <div className="menu-container">
+        <img src={Pizza} alt="pizza" />
+        <h2>Whats up though</h2>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Menu;
