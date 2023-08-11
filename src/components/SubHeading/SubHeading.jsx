a41f19a1659da91b505a1b18d34c59fb51c4d8cb
Cleve <clevemomanyi@gmail.com>
2023-08-11 15:01:30 +0300

    home done

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { images } from "../../constants"
 
-const SubHeading = () => (
+const SubHeading = ({title}) => (
   <div>
-    <p className='p__cormorant'>TITLE</p>
+    <p className='p__cormorant'>{title}</p>
     <img src={images.spoon} alt="spoon" />
   </div>
 );
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 
 import "./Header.css";
-import images from "../../constants";
+import images from "../../constants/images";
 import { SubHeading } from "../../components";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavor" />
       <h1 className="app__header_h1"> The Key to fine dining</h1>
+      <p className="p__opensans" style={{ margin: "2em 0em" }}>
+        Experience <span>Geritch:</span> Where Taste Meets Excellence. Join us
+        for an unforgettable culinary journey curated by our passionate chefs.
+      </p>
+      <button className="custom__button" type="button">
+        Explore More
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome" />
+    </div>
   </div>
 );
 
