a5ff617b2f6ce640646487942082a54f3cacf735
Cleve <clevemomanyi@gmail.com>
2023-08-11 15:11:15 +0300

    home done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
-import "./Header.css";
-import images from "../../constants/images";
 import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavor" />
-      <h1 className="app__header_h1"> The Key to fine dining</h1>
-      <p className="p__opensans" style={{ margin: "2em 0em" }}>
-        Experience <span>Geritch:</span> Where Taste Meets Excellence. Join us
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Experience <span>Geritch</span>: Where Taste Meets Excellence. Join us
         for an unforgettable culinary journey curated by our passionate chefs.
       </p>
-      <button className="custom__button" type="button">
-        Explore More
+      <button type="button" className="custom__button">
+        Explore Menu
       </button>
     </div>
+
     <div className="app__wrapper_img">
-      <img src={images.welcome} alt="welcome" />
+      <img src={images.welcome} alt="header_img" />
     </div>
   </div>
 );
