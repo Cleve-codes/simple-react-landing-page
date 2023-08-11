faf991b456dd72bcf59e95f7506950837a11c568
Cleve <clevemomanyi@gmail.com>
2023-08-11 14:33:16 +0300

    home cmp

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { images } from "../../constants"
 
 const SubHeading = () => (
   <div>
-    SubHeading
+    <p className='p__cormorant'>TITLE</p>
+    <img src={images.spoon} alt="spoon" />
   </div>
 );
 
