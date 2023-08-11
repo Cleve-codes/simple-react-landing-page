84927ab61d1b6ec50b604dc6bccf5e866475501e
Cleve <clevemomanyi@gmail.com>
2023-08-11 11:31:50 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./index.css";
 
-import Home from "./components/Home";
-import Menu from "./components/Menu";
-import videoBg from "./Assets/bgVid3.mp4";
-
 function App() {
-
-  return (
-    <div className={`App`}>
-      <div className="overlay"></div>
-      <video src={videoBg} autoPlay loop muted />
-      <Home />
-      <Menu />
-    </div>
-  );
+  return <div className={`App`}></div>;
 }
 
 export default App;
