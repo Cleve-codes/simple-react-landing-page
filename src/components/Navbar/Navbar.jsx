68c68d5f74b66c945e5772a1bd2a397bb0321138
Cleve <clevemomanyi@gmail.com>
2023-08-11 13:59:39 +0300

    navbar nearer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 
@@ -6,6 +6,15 @@ import "./Navbar.css";
 import images from "../../constants/images";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleSmallScreen = () => {
+    setToggleMenu((prev) => {
+      return !prev
+    });
+    
+  };
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -29,9 +38,53 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="app__navbar-login">
-          <a href="#login" className="p__opensans">Login / Register</a>
-          <div />
-          <a href="/" className="p__opensans">Book Table</a>
+        <a href="#login" className="p__opensans">
+          Login / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      {/* Small screen Navbar */}
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => {
+            handleSmallScreen();
+          }}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex_center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                handleSmallScreen();
+              }}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
